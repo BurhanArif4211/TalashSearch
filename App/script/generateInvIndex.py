00f8eac20c6d247754dbd09062476eb6d465641d
pc <pc@DESKTOP-U4KSOD2>
2025-11-06 00:01:06 +0500

    inverse index script in progress

--- App/script/generateInvIndex.py
+++ App/script/generateInvIndex.py
@@ -1,2 +1,38 @@
+from LughaatNLP import LughaatNLP 
+import mysql.connector
 
+def setup_database():
+    """Create database and tables if they don't exist"""
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database = "urdu_search_engine"
+        )
+        
+        return conn
+        
+    except Exception as e:
+        print(f"Error setting up database: {e}")
+        return None
+
+# main action
+
+lughaat = LughaatNLP() 
+
+connection = setup_database()
+cursor = connection.cursor()
+
+cursor.execute("SELECT full_text FROM documents;")
+hugeUrduData = cursor.fetchall()
+
+hugeUrduData[0][0] ## this accesses the text of first document. data is fetched from the table in this format: [('urdu text...'),(...),(...),...]
+
+# TODO process the urdu text in every document by normalizing stopword removal and tokenize it and build a reverse index which is easy and fast to search 
+# important functions that Lughaat NLP gives
+# lughaat.remove_stopwords(text)
+# lughaat.urdu_tokenize(text)
+# lughaat.lemmatize_sentence(sentence)
+# lughaat.urdu_stemmer(sentence)
 
